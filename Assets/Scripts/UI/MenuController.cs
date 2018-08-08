6fcda44cbcbff1d4e7a6b4959697fb1744c5422a
Amon <a.foxbrim@gmail.com>
2018-08-07 21:40:26 -0400

    Added pause menu

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,46 @@
+﻿using Assets.Scripts.Systems;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject PauseMenu;
+
+    public void Start( )
+    {
+        if( PauseMenu == null )
+        {
+            Debug.LogWarning( "Warning: Pause Menu not set to controller" );
+            enabled = false;
+        }
+    }
+
+
+    public void PauseGame()
+    {
+        //PauseMenu.PauseGame( );
+    }
+
+    public void ResumeGame( )
+    {
+       // IsPaused = false;
+        World.Active.GetExistingManager<MovementSystem>( ).Enabled = true;
+    }
+
+    public void ExitToMainMenu( )
+    {
+        SceneManager.LoadScene( "MainMenu" );
+    }
+
+
+    public void Update( )
+    {
+        var escape = Input.GetKeyUp( KeyCode.Escape );
+
+        if( escape )
+        {
+        }
+    }
+
+}
